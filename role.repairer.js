74f3d94e39aee555bc2c11a178932e79d4090d9c
Lik Wei <likweitan@gmail.com>
2021-07-04 21:01:00 +0800

    Revert Changes

--- role.repairer.js
+++ role.repairer.js
@@ -12,23 +12,15 @@ var roleRepairer = {
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (object) => object.hits < object.hitsMax / 4,
       });
-      if (creep.memory.target.hits > creep.memory.target.hitsMax / 4) {
+      console.log("REPAIRING:" + targets);
+      targets.sort((a, b) => a.hits - b.hits);
+
+      if (targets.length > 0) {
         if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, {
+          creep.memory.target = targets[0];
+          creep.moveTo(targets[0], {
             visualizePathStyle: { stroke: "#ffaa00" },
           });
-        }
-      } else {
-        console.log("REPAIRING:" + targets);
-        targets.sort((a, b) => a.hits - b.hits);
-
-        if (targets.length > 0) {
-          if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-            creep.memory.target = targets[0];
-            creep.moveTo(targets[0], {
-              visualizePathStyle: { stroke: "#ffaa00" },
-            });
-          }
         }
       }
     } else {
